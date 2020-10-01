56a0e00cf206d22fa960ee46ea424f4292b1332b
jasurn <97jasur@gmail.com>
2020-10-02 02:18:57 +0500

    add compiled files because memory overflow on server

---
+++ robofriends-front/build/precache-manifest.f6ff27685434f131dfd60f46159a7602.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fb8dfc5e8b81b78d11cefa2088fc587",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8191e4ba02f2b654e67",
+    "url": "/static/css/main.ea043322.chunk.css"
+  },
+  {
+    "revision": "45ff908cf1a627999239",
+    "url": "/static/js/2.882173da.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.882173da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8191e4ba02f2b654e67",
+    "url": "/static/js/main.1658192c.chunk.js"
+  },
+  {
+    "revision": "4178a590d621709143ec",
+    "url": "/static/js/runtime-main.40c3c141.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
